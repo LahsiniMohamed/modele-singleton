157dfa04dc4d362a745cabaf5425fe4ff782c9dc
LahsiniMohamed <36932931+LahsiniMohamed@users.noreply.github.com>
2020-12-13 15:02:27 +0100

    correction des erreurs

--- terre.java
+++ terre.java
@@ -1,65 +1,31 @@
-package singleton;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.*; 
 
-public class terre{
-    final static Semaphore semaphore = new Semaphore(1,true);
-    private static int instance = 0;
-    int age;
-    int population;
-    static terre ter;
-    private terre(int age, int population){
-        this.age = age;
+public class Terre {
+    private static Terre terre = null;
+    private  int weight;
+    private  int population ; 
+    private static Semaphore sem  = new Semaphore(1);
+    private Terre(int weight , int population){
+        this.weight = weight;
         this.population = population;
     }
-    public static terre create(int age, int population){
-        ter = null;
-        class Local implements Callable<terre>{
-        public terre call() throws Exception{
-                Thread.sleep(2000);
-                if (instance==0){
-                    ter = new terre(age, population);
-                    instance+=1;
-                    return ter;       
-                }
-                else {
-                    ter = null;
-                    return ter;
-                }
-        }   
-    }
-    terre result;
+    public static Terre getTerre(){
     try{
-        
-    semaphore.acquire(1);
-
-    ExecutorService executor = Executors.newFixedThreadPool(1);
-    Callable<terre> callable = new Local();
-    Future<terre> future = executor.submit(callable);
-    try {
-        //print the return value of Future, notice the output delay in console
-        // because Future.get() wai,ts for task to get completed
-        result = future.get();
-        executor.shutdown();
-
-    } catch (InterruptedException | ExecutionException e) {
-        result= null;
-    }
-    semaphore.release(1);
-    return result;
-}
-catch(final InterruptedException ie){
-    System.out.println("there is an error occured");
-    return null;
-}
-    
-}
+        System.out.println("aquiring semaphore ....... \n");
+        sem.acquire();
+        if (terre == null){
+            System.out.println("Construction du Singleton au premier appel");
+            terre = new Terre(150 , 7);
+            }
+        }
+        catch (InterruptedException exc) { 
+            System.out.println(exc); 
+        } 
+        System.out.println("releasing semaphore ....... \n");
+        sem.release();
+        return terre ;
+    };
+    public int getWeight(){return this.weight;};
+    public int getPopulation(){return this.population;};
 
 }
-
