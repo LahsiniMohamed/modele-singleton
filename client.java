1826b4de21bd5d75dd6816e0f2c346264779fe53
LahsiniMohamed <36932931+LahsiniMohamed@users.noreply.github.com>
2020-12-13 15:01:58 +0100

    correction des erreurs

--- client.java
+++ client.java
@@ -1,37 +1,36 @@
-package singleton;
-public class client extends Thread{
+public class Clients extends Thread{
     public static void main(String[] args) {
-        client1 request1 = new client1();
-        client2 request2 = new client2();
-        client3 request3 = new client3();
+        Clients1 request1 = new Clients1();
+        Clients2 request2 = new Clients2();
+        Clients3 request3 = new Clients3();
     
        request1.start();
-       System.out.print("client 1 request is running \n");
+       System.out.print("Clients 1 request is running \n");
        request2.start();
-       System.out.print("client 2 request is running\n");
+       System.out.print("Clients 2 request is running\n");
        request3.start();
-       System.out.print("client 3 request is running\n");    
+       System.out.print("Clients 3 request is running\n");    
     }
 }
 
-class client2 extends Thread{
-    terre terre2;
+class Clients2 extends Thread{
+    Terre terre2;
     public void run(){
-        terre2 = terre.create(2,2);
-        System.out.println("client 2 / terre = " + terre2);
+        terre2 = Terre.getTerre();
+        System.out.println("Clients 2 / Terre = " + terre2);
     }
 }
-class client1 extends Thread{
-        terre terre1;
+class Clients1 extends Thread{
+        Terre Terre1;
         public void run(){
-            terre1 = terre.create(1,1);
-            System.out.println("client 1 / terre = " + terre1);
+            Terre1 = Terre.getTerre();
+            System.out.println("Clients 1 / Terre = " + Terre1);
         }  
     }  
-class client3 extends Thread{
-        terre terre3;
+class Clients3 extends Thread{
+        Terre Terre3;
         public void run(){
-            terre3 = terre.create(1,3);
-            System.out.println("client 3 / terre = " + terre3);
+            Terre3 = Terre.getTerre();
+            System.out.println("Clients 3 / Terre = " + Terre3);
         }  
     }     
